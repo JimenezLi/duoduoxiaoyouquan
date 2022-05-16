8cba8790577902ef12c9565b4e09edea7e925f95
JimenezLi <jimenezli@foxmail.com>
2022-05-16 20:32:19 +0800

    Add potion effect to ItemChiGua

--- src/main/java/jimenezli/zju/duoduoxiaoyouquan/item/expressions/ItemChiGua.java
+++ src/main/java/jimenezli/zju/duoduoxiaoyouquan/item/expressions/ItemChiGua.java
@@ -3,9 +3,11 @@ package jimenezli.zju.duoduoxiaoyouquan.item.expressions;
 import jimenezli.zju.duoduoxiaoyouquan.Reference;
 import jimenezli.zju.duoduoxiaoyouquan.duoduoxiaoyouquan;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
@@ -23,6 +25,6 @@ public class ItemChiGua extends ItemFood {
 
     @Override
     protected void onFoodEaten(ItemStack stack, World world, EntityPlayer player) {
-
+        player.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 60, 2));
     }
 }
