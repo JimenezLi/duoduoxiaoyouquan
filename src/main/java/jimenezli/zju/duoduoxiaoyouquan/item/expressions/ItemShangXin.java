65e053326fab374e3f784e347e4217db2e037765
JimenezLi <jimenezli@foxmail.com>
2022-05-21 00:06:01 +0800

    Add usage of ItemShangXin

--- src/main/java/jimenezli/zju/duoduoxiaoyouquan/item/expressions/ItemShangXin.java
+++ src/main/java/jimenezli/zju/duoduoxiaoyouquan/item/expressions/ItemShangXin.java
@@ -2,13 +2,41 @@ package jimenezli.zju.duoduoxiaoyouquan.item.expressions;
 
 import jimenezli.zju.duoduoxiaoyouquan.Reference;
 import jimenezli.zju.duoduoxiaoyouquan.duoduoxiaoyouquan;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Vector;
 
 public class ItemShangXin extends Item {
-    private static String name = "shang_xin";
     public ItemShangXin(){
+        String name = "shang_xin";
         this.setRegistryName(Reference.MODID + ":" + name);
         this.setUnlocalizedName(Reference.MODID + "." + name);
         this.setCreativeTab(duoduoxiaoyouquan.DUODUO);
+    }
+
+    @Nonnull
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack shang_xin = playerIn.getHeldItem(handIn);
+        if (!playerIn.capabilities.isCreativeMode) {
+            shang_xin.shrink(1);
+        }
+        worldIn.setRainStrength(worldIn.getRainStrength(1.0F) + 1.0F);
+
+        playerIn.addStat(Objects.requireNonNull(StatList.getObjectUseStats(this)));
+        return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, shang_xin);
     }
 }
