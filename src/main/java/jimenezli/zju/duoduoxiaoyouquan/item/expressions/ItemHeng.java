8bfc5327329c3f7c38482de00a2da0ef29731d57
JimenezLi <jimenezli@foxmail.com>
2022-05-21 21:25:13 +0800

    Add usage of ItemHeng

--- src/main/java/jimenezli/zju/duoduoxiaoyouquan/item/expressions/ItemHeng.java
+++ src/main/java/jimenezli/zju/duoduoxiaoyouquan/item/expressions/ItemHeng.java
@@ -2,13 +2,51 @@ package jimenezli.zju.duoduoxiaoyouquan.item.expressions;
 
 import jimenezli.zju.duoduoxiaoyouquan.Reference;
 import jimenezli.zju.duoduoxiaoyouquan.duoduoxiaoyouquan;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
 
 public class ItemHeng extends Item {
-    private static String name = "heng";
     public ItemHeng(){
+        String name = "heng";
         this.setRegistryName(Reference.MODID + ":" + name);
         this.setUnlocalizedName(Reference.MODID + "." + name);
         this.setCreativeTab(duoduoxiaoyouquan.DUODUO);
+    }
+
+    @Nonnull
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack heng = playerIn.getHeldItem(handIn);
+        if (!playerIn.capabilities.isCreativeMode) {
+            heng.shrink(1);
+        }
+
+        for (Entity entity: worldIn.loadedEntityList) {
+            if (entity instanceof EntityLiving && !entity.equals(playerIn)) {
+                float distance = entity.getDistance(playerIn);
+                if (distance < 10.0F) {
+                    entity.attackEntityFrom(DamageSource.GENERIC, 1.0F);
+                    playerIn.heal(1.0F);
+                }
+            }
+        }
+
+        playerIn.addStat(Objects.requireNonNull(StatList.getObjectUseStats(this)));
+        return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, heng);
     }
 }
