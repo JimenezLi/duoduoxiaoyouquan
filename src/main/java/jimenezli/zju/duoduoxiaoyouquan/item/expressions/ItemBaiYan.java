2529de0411f76f2d60674ca2e68f4e40cfab6b05
JimenezLi <jimenezli@foxmail.com>
2022-05-23 16:03:57 +0800

    Add usage of ItemBaiYan

--- src/main/java/jimenezli/zju/duoduoxiaoyouquan/item/expressions/ItemBaiYan.java
+++ src/main/java/jimenezli/zju/duoduoxiaoyouquan/item/expressions/ItemBaiYan.java
@@ -2,13 +2,56 @@ package jimenezli.zju.duoduoxiaoyouquan.item.expressions;
 
 import jimenezli.zju.duoduoxiaoyouquan.Reference;
 import jimenezli.zju.duoduoxiaoyouquan.duoduoxiaoyouquan;
+import net.minecraft.entity.item.EntityEnderPearl;
+import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
 
 public class ItemBaiYan extends Item {
-    private static String name = "bai_yan";
     public ItemBaiYan(){
+        String name = "bai_yan";
         this.setRegistryName(Reference.MODID + ":" + name);
         this.setUnlocalizedName(Reference.MODID + "." + name);
         this.setCreativeTab(duoduoxiaoyouquan.DUODUO);
+        this.setMaxDamage(64);
+        this.setMaxStackSize(1);
+    }
+
+    //Copied from ItemEnderPearl
+    @Nonnull
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+
+        if (!playerIn.capabilities.isCreativeMode)
+        {
+            itemstack.damageItem(1, playerIn);
+        }
+
+        worldIn.playSound((EntityPlayer)null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.ENTITY_ENDERPEARL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+        playerIn.getCooldownTracker().setCooldown(this, 20);
+
+        if (!worldIn.isRemote)
+        {
+            EntityEnderPearl entityenderpearl = new EntityEnderPearl(worldIn, playerIn);
+            entityenderpearl.shoot(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0.0F, 1.5F, 1.0F);
+            worldIn.spawnEntity(entityenderpearl);
+        }
+
+        playerIn.addStat(StatList.getObjectUseStats(this));
+        return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
     }
 }
