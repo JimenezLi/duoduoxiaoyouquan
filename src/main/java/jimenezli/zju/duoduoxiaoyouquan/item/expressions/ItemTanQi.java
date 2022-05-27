084c8886efa1e47fe0f11d5577a15095fc8e02f1
JimenezLi <jimenezli@foxmail.com>
2022-05-27 12:05:29 +0800

    Add usage of ItemTanQi

--- src/main/java/jimenezli/zju/duoduoxiaoyouquan/item/expressions/ItemTanQi.java
+++ src/main/java/jimenezli/zju/duoduoxiaoyouquan/item/expressions/ItemTanQi.java
@@ -2,13 +2,50 @@ package jimenezli.zju.duoduoxiaoyouquan.item.expressions;
 
 import jimenezli.zju.duoduoxiaoyouquan.Reference;
 import jimenezli.zju.duoduoxiaoyouquan.duoduoxiaoyouquan;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
 
 public class ItemTanQi extends Item {
-    private static String name = "tan_qi";
     public ItemTanQi(){
+        String name = "tan_qi";
         this.setRegistryName(Reference.MODID + ":" + name);
         this.setUnlocalizedName(Reference.MODID + "." + name);
         this.setCreativeTab(duoduoxiaoyouquan.DUODUO);
+    }
+
+    @Nonnull
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack tan_qi = playerIn.getHeldItem(handIn);
+        if (!playerIn.capabilities.isCreativeMode) {
+            tan_qi.shrink(1);
+        }
+
+        if (!worldIn.isRemote) {
+            for (Entity entity: worldIn.loadedEntityList) {
+                if (entity instanceof EntityLivingBase && !(entity instanceof EntityPlayer)) {
+                    if (entity.getDistance(playerIn) < 20.0F) {
+                        ((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, 200, 1));
+                    }
+                }
+            }
+        }
+
+        playerIn.addStat(Objects.requireNonNull(StatList.getObjectUseStats(this)));
+        return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, tan_qi);
     }
 }
