1fe091ec1f5e942951b116bb2f86890bbb589b87
JimenezLi <jimenezli@foxmail.com>
2023-02-08 15:59:23 +0800

    Simplify register code

--- src/main/java/jimenezli/zju/duoduoxiaoyouquan/register/ModelsRegister.java
+++ src/main/java/jimenezli/zju/duoduoxiaoyouquan/register/ModelsRegister.java
@@ -18,37 +18,9 @@ public class ModelsRegister {
         registerModel(ItemsRegister.yunduoduo_core);
         registerModel(ItemsRegister.yunduoduo);
 
-        registerModel(ItemsRegister.bai_yan);
-        registerModel(ItemsRegister.chi_gua);
-        registerModel(ItemsRegister.ci_ya);
-        registerModel(ItemsRegister.dian_zan);
-        registerModel(ItemsRegister.fen);
-        registerModel(ItemsRegister.fen_nu);
-        registerModel(ItemsRegister.gan_ga);
-        registerModel(ItemsRegister.gou_tou);
-        registerModel(ItemsRegister.heng);
-        registerModel(ItemsRegister.he_kuo_luo);
-        registerModel(ItemsRegister.hua_ji);
-        registerModel(ItemsRegister.jia_you);
-        registerModel(ItemsRegister.jing_dai);
-        registerModel(ItemsRegister.kai_xin);
-        registerModel(ItemsRegister.ke_lian);
-        registerModel(ItemsRegister.ma_le);
-        registerModel(ItemsRegister.mei_yan_kan);
-        registerModel(ItemsRegister.mo_tou);
-        registerModel(ItemsRegister.qing_zhu);
-        registerModel(ItemsRegister.qin_qin);
-        registerModel(ItemsRegister.qi_fu);
-        registerModel(ItemsRegister.ren_pin);
-        registerModel(ItemsRegister.san_hua);
-        registerModel(ItemsRegister.shang_xin);
-        registerModel(ItemsRegister.tan_qi);
-        registerModel(ItemsRegister.tou_tu);
-        registerModel(ItemsRegister.tui_yan_jing);
-        registerModel(ItemsRegister.tu_xie);
-        registerModel(ItemsRegister.xiao_chou);
-        registerModel(ItemsRegister.xiao_ku);
-        registerModel(ItemsRegister.yi_wen);
+        for (Item item: ItemsRegister.expressionList) {
+            registerModel(item);
+        }
 
         registerModel(ItemsRegister.charged_xiao_ku);
     }
